110b7d30ae48ca161b09056f8e4c45374603ed48
Lars M. Angelsen <larsm.angelsen@gmail.com>
2017-11-20 18:05:20 +0100

    Music control from node 1 now works

--- node_3/control.ino
+++ node_3/control.ino
@@ -1,48 +1,69 @@
 //Control function, changes volume, song etc
+void ISR0()
+{
+  bool CTRL_MODE = digitalRead(INPUT_PIN_0);
+  bool DIRECTION = digitalRead(INPUT_PIN_1);
+
+  //Update last message
+  if (CTRL_MODE)
+  {
+    if (DIRECTION) last_message = PLAY_STARTUP;
+    else last_message = PLAY_END;
+  }
+  else
+  {
+    if (DIRECTION) last_message = NEXT_SONG;
+    else last_message = PREV_SONG;
+  }
+  
+}
+
 void listenForData()
 {
-  //Filter out repeating interrupt
-  noInterrupts();
-  delay(200);
-  interrupts();
-  
+  Serial.println("DATA RECEIVED");
   //Starts game if not started yet
   if (!start)
   {
     start = 1;
     return;
   }
-  
 
-  //DEBUG
-  Serial.println("INTERRUPT");
-
-  
-  bool CTRL_MODE = digitalRead(INPUT_PIN_0);
-  bool DIRECTION = digitalRead(INPUT_PIN_1);
-
-  if (CTRL_MODE) //Startup and end game
+  switch (last_message)
   {
-    if (DIRECTION)
+    case (NO_MESSAGE):
     {
-      play_song(3); //Play startup sound
+      Serial.println("NO_MESSAGE");
+      return;
     }
-    else
+    case (PLAY_STARTUP):
     {
-      play_song(4); //Play end game sound
+      last_message = NO_MESSAGE;
+      Serial.println("PLAY_STARTUP");
+      play_song(songList[3]);
+      return;
     }
-  }
-  else
-  {
-    if (DIRECTION) //Increase
+    case (PLAY_END):
     {
-      if (current_song < SONG_COUNT-1) current_song++;
-      else current_song = 0;
+      last_message = NO_MESSAGE;
+      Serial.println("PLAY_END");
+      play_song(songList[4]);
+      return;
     }
-    else  //Decrease
+    case (NEXT_SONG):
     {
+      last_message = NO_MESSAGE;
+      Serial.println("NEXT_SONG");
+      if (current_song < SONG_COUNT - 1) current_song++;
+      return;
+    }
+    case (PREV_SONG):
+    {
+      last_message = NO_MESSAGE;
+      Serial.println("PREV_SONG");
       if (current_song > 0) current_song--;
-      else current_song = SONG_COUNT;
+      return;
     }
   }
+  
+  
 }
--- node_3/node_3.ino
+++ node_3/node_3.ino
@@ -26,7 +26,7 @@
 #define  a5s   1073
 #define  b5    1012
 #define  c6    955
-#define  R     9999        //REST
+#define  R     0        //REST
 
 //List of songs
 #define SONG_COUNT 3 //2 songs, 1 mute, and 2 game sounds
@@ -35,15 +35,23 @@ unsigned int songList[SONG_COUNT+2][SONG_LENGTH]=
 {
   /*STARWARS IMPERIAL MARCH */  {a4,500, R,50 ,a4,500, R,50 ,a4,500 ,f4,400 ,c5,200 ,a4,600 ,f4,400 ,c5,200 ,a4,600 ,e5,500 ,e5,500 ,e5,500 ,f5,400 ,c5,200 ,g5,600 ,f5,400 ,c5,200 ,a4,600}
   /*STARWARS THEME */         ,{f4,210, f4,210, f4,210,  a4s,1280,   f5,1280,  d5s,210,  d5,210,  c5,210, a5s,1280, f5,640, d5s,210,  d5,210,  c5,210, a5s,1280, f5,640, d5s,210, d5,210, d5s,210,c5,1280, R,0}
-  /*MUTE*/                    ,{R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0}
-  /* STARTUP */               ,{c4,100 ,a4,100 ,c5,400 ,d5s,500 ,e4,150 ,R,10000 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0}
-  /* END GAME */              ,{e4,800 ,e3,1200 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0}
+  /*MUTE*/                    ,{R,1000 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0}
+  /* STARTUP */               ,{c4,100 ,a4,100 ,c5,400 ,d5s,500 ,e4,150 ,R,10000 ,R,500 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0}
+  /* END GAME */              ,{e4,800 ,e3,1200 ,R,5000 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0 ,R,0}
 };
 
 //Music variables
 int current_song;
 int volume;
 int start;
+
+//Communication variable
+#define NO_MESSAGE 0
+#define PLAY_STARTUP 1
+#define PLAY_END 2
+#define NEXT_SONG 3
+#define PREV_SONG 4
+int last_message;
 
 //Control pins
 #define INTERRUPT_PIN 3
@@ -54,12 +62,14 @@ int start;
 void setup()
 {
   //Setup variables
-  volume = 255;
   current_song = 0;
   start = 0;
 
+  //LAST MESSAGE
+  last_message = NO_MESSAGE;
+
   //Setup interrupt pin
-  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), listenForData, RISING);
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), ISR0, RISING);
 
   //Setup other pins
   pinMode(INPUT_PIN_0,INPUT);
@@ -74,18 +84,27 @@ void setup()
   
 
   //Waits for node 1 to enable music
-  while (!start){Serial.println("...");}
+  while (!start)
+  {
+    Serial.println("...");
+    if (last_message != NO_MESSAGE)
+    {
+      last_message = NO_MESSAGE;
+      break;
+    }
+  }
   
   //Plays startup sound
   play_song(songList[3]);
   Serial.println("SETUP COMPLETED");
   delay(3000);
-  current_song = 0;
 }
 
 void loop()
 {
-  Serial.println(current_song);
   //Main operating loop, always listens for commands with interrupt pin
   play_song(songList[current_song]);
+
+  //Check for message from node 1
+  if (last_message != NO_MESSAGE) listenForData();
 }
